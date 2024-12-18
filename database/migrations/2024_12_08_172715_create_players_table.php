646130a635ed3f11f325081e00cb82516118a5df
Mohammad Tarek <tarek3529@gmail.com>
2024-12-18 20:24:43 +0200

    remove team_id from players table

--- database/migrations/2024_12_08_172715_create_players_table.php
+++ database/migrations/2024_12_08_172715_create_players_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('players', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('team_id');
             $table->timestamps();
         });
     }
