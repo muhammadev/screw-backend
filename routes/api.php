2ebec45b71e3819abc4c01c9f7ff9bd785fe8e5a
Mohammad Tarek <tarek3529@gmail.com>
2024-12-23 19:35:54 +0200

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\PlayerController;
 use App\Http\Controllers\TeamController;
+use App\Http\Controllers\GameTypeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,3 +17,5 @@ Route::apiResource('games', GameController::class);
 Route::apiResource('players', PlayerController::class);
 
 Route::apiResource('teams', TeamController::class);
+
+Route::apiResource('game-types', GameTypeController::class);
