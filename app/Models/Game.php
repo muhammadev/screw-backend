973311b775b8bd84a9c158a3cc52b419fb7ce97e
Mohammad Tarek <tarek3529@gmail.com>
2024-12-23 19:34:22 +0200

    update game model

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -7,14 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
+    protected $fillable = ['game_type_id'];
+
     public function players()
     {
         return $this->belongsToMany(Player::class)
-            ->withPivot("score", "winner", "screwed");
+            ->withPivot("score", "winner", "screwed")
+            ->withTimestamps();
     }
 
-    public function getTestAttribute()
+    public function teams()
     {
-        return 'test';
+        return $this->belongsToMany(Team::class, 'game_team')
+            ->withPivot(['score', 'winner', 'screwed'])
+            ->withTimestamps();
+    }
+
+    public function gameType() {
+        return $this->belongsTo(GameType::class, 'game_type_id');
     }
 }
